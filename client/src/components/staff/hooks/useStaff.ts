f08e589ba215a0717a1a4a91f83ec0a036aea1cd
sleeplesseditor <josh@bryson.me.uk>
2024-02-18 18:14:57 +0000

    useStaff query added

--- client/src/components/staff/hooks/useStaff.ts
+++ client/src/components/staff/hooks/useStaff.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useQuery } from "@tanstack/react-query";
 import type { Staff } from "@shared/types";
 
 import { filterByTreatment } from "../utils";
@@ -8,17 +8,20 @@ import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
 // query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 export function useStaff() {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
+  const fallbackData: Staff[] = [];
 
-  // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
+  const { data: staff = fallbackData} = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  });
 
   return { staff, filter, setFilter };
 }
