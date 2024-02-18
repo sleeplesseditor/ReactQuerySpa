fdea5e9f88c33dcce9e2c8d29bfc58a2021f019f
sleeplesseditor <josh@bryson.me.uk>
2024-02-18 17:56:08 +0000

    Updated client folder to reflect new course code and implemented queryClient setup

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,21 @@
-import type { Treatment } from '../../../../../shared/types';
-import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+import type { Treatment } from "@shared/types";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/axiosInstance";
+import { queryKeys } from "@/react-query/constants";
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallbackData: Treatment[] = [];
+
+  const { data = fallbackData } = useQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments
+  });
+
+  return data;
 }
